d9fd218a6f2763783cdd3622f4eb34e37dc22e65
maraloedev <martinsonsecaeduardo@gmail.com>
2025-08-21 20:19:36 +0200

    CURSO FIREBASE BASIC

---
+++ CursoFirebaseLite/app/src/main/java/com/maraloedev/cursofirebaselite/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.maraloedev.cursofirebaselite
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.maraloedev.cursofirebaselite.ui.theme.CursoFirebaseLiteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CursoFirebaseLiteTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(paddingValues = innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier.clickable{ throw RuntimeException("Hola") }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CursoFirebaseLiteTheme {
+        Greeting("Android")
+    }
+}
